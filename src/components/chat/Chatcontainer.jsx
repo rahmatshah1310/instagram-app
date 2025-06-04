3129fa9d1e2bb9676a2dd5a04e7ecbedd0796a03
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-04 12:34:15 +0500

    feeat:import all elements in the chatcontainer

---
+++ src/components/chat/Chatcontainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Mic, ImageIcon, Smile, Heart, Info } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useChat } from "@/features/context/ChatContext";
+import ChatInput from "@/components/chat/ChatInput";
+
+const Chatcontainer = () => {
+  return (
+    <div>Chatcontainer</div>
+  )
+}
+
+export default Chatcontainer
